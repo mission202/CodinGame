9b890db9f10f86d8b94633b1c9f7f5a522e34f6a
Rob Cooper <robc.the.geek@gmail.com>
2018-02-17 11:51:45 +0000

    Adding Tests

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace CodinGame.App
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+public class Solution
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int n = int.Parse(Console.ReadLine()); // the number of adjacency relations
+        for (int i = 0; i < n; i++)
         {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int xi = int.Parse(inputs[0]); // the ID of a person which is adjacent to yi
+            int yi = int.Parse(inputs[1]); // the ID of a person which is adjacent to xi
         }
+
+        var people = new Dictionary<int, int[]>();
+
+
+        // The minimal amount of steps required to completely propagate the advertisement
+        Console.WriteLine("1");
     }
 
-    public static class Solution
+    public static int Find(string[] input)
     {
-        public static bool Works => true;
+        return 2;
     }
 }
--- CodinGame.Tests/Tests.cs
+++ CodinGame.Tests/Tests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CodinGame.App;
 using Xunit;
 
 namespace CodinGame.Tests
@@ -11,9 +10,21 @@ namespace CodinGame.Tests
     public class Tests
     {
         [Fact]
-        public void TestsWork()
+        public void Test1()
         {
-            Assert.True(Solution.Works);
+            Assert.Equal(2, Solution.Find("0 1|1 2|2 3|2 4".Split('|')));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2, Solution.Find("0 1|1 2|1 4|2 3|4 5|4 6".Split('|')));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(3, Solution.Find("0 1|0 8|0 15|1 2|1 5|2 3|2 4|5 6|5 7|8 9|8 12|9 10|9 11|12 13|12 14|15 16|15 19|16 17|16 18|19 20|19 21".Split('|')));
         }
     }
 }
