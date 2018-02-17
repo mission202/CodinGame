8ac92cc960a150ec218d9ec8532bf45301b8b528
Rob Cooper <robc.the.geek@gmail.com>
2018-02-17 13:49:52 +0000

    Up to Test 4

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -71,31 +71,28 @@ public static class Extensions
 
     public static int CountLayers(this Dictionary<int, HashSet<int>> graph, int startAt)
     {
+        Console.Error.WriteLine($"Counting layers in Graph of {graph.Keys.Count} items from Node {startAt}");
+
         var visited = new HashSet<int>();
         var toSearch = new Queue<Tuple<int, int>>();
         var result = 0;
 
-        visited.Add(startAt);
         toSearch.Enqueue(Tuple.Create(startAt, 0));
 
         while (toSearch.Count > 0)
         {
-            // Pop
             var n = toSearch.Dequeue();
-
-            // Level Up (If Deeper)
             if (n.Item2 > result) result = n.Item2;
-
-            // Visit First
             visited.Add(n.Item1);
 
-            // Queue Unvisited Connected
-            Console.Error.WriteLine($"Getting Connected Items for: {n.Item1}");
             var connected = graph[n.Item1];
-            var nextUnvisited = connected.Where(k => !visited.Contains(k)).ToList();
+            var unvisited = connected.Where(k => !visited.Contains(k)).ToList();
 
-            if (nextUnvisited.Any())
-                toSearch.Enqueue(Tuple.Create(nextUnvisited.First(), n.Item2 + 1));
+            foreach (var adjacent in unvisited)
+            {
+                visited.Add(adjacent);
+                toSearch.Enqueue(Tuple.Create(adjacent, n.Item2 + 1));
+            }
         }
 
         return result;
