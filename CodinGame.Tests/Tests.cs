472d530208e01bbc8b321cb63a8cdea3daa5f691
Rob Cooper <robc.the.geek@gmail.com>
2018-02-17 06:12:37 +0000

    Clean Slate

---
+++ CodinGame.Tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodinGame.App;
+using Xunit;
+
+namespace CodinGame.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestsWork()
+        {
+            Assert.True(Solution.Works);
+        }
+    }
+}
