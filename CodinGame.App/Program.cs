724f29de582ae89f342541b9366ce8ca0b332adc
Rob Cooper <robc.the.geek@gmail.com>
2018-02-20 20:08:41 +0000

    Refactored and Submitted

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -1,26 +1,15 @@
 using System;
 using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
-
-public static class D
-{
-    public static Action<string> Print = message => Console.Error.WriteLine(message);
-    public static Action<string> Write = message => Console.WriteLine(message);
-    public static Func<string> Read = () => Console.ReadLine();
-    public static Func<int> ReadInt = () => int.Parse(Read());
-}
 
 public class Solution
 {
+    static Func<int> ReadInt = () => int.Parse(Console.ReadLine());
+
     static void Main(string[] args)
     {
-        int R = D.ReadInt();
-        int L = D.ReadInt();
 
-        D.Write(ConwaySequence.Find(R, L));
+        Console.WriteLine(ConwaySequence.Find(ReadInt(), ReadInt()));
     }
 }
 
@@ -36,8 +25,6 @@ public static class ConwaySequence
         for (int currentLine = 1; currentLine < targetLine; currentLine++)
         {
             var line = lines.Last();
-            D.Print($"Encoding Line {currentLine}: {string.Join(" ", line)}");
-
             var encoded = new List<int>();
             var count = 0;
 
@@ -48,16 +35,12 @@ public static class ConwaySequence
 
                 if (i + 1 == line.Length || line[i + 1] != current)
                 {
-                    // Encode and close this 'block'
-                    D.Print($"Encoding: {count} * {current}");
                     encoded.AddRange(current.Encode(count));
                     count = 0;
                 }
             }
 
             lines.Add(encoded.ToArray());
-            D.Print("Current State");
-            D.Print(lines.Pretty());
         }
 
         return lines.Last().SpacedOut(); // o_O
@@ -69,16 +52,6 @@ public static class Extensions
     public static string SpacedOut(this IEnumerable<int> chars)
     {
         return string.Join(" ", chars);
-    }
-
-    public static string SpacedOut(this IEnumerable<int[]> lines)
-    {
-        return string.Join(" ", lines.Select(l => l.SpacedOut()));
-    }
-
-    public static string Pretty(this IEnumerable<int[]> lines)
-    {
-        return string.Join(Environment.NewLine, lines.Select(l => l.SpacedOut()));
     }
 
     public static int[] Encode(this int @int, int count)
