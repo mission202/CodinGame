9b18cddd843cfd235568d5db32625b960b3c2f57
Rob Cooper <robc.the.geek@gmail.com>
2018-02-20 09:43:24 +0000

    Tests Passing!

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -16,47 +16,26 @@ public static class Solution
 
     public static long Find(IEnumerable<string> input)
     {
-        // Thanks http://fooplot.com for the visualisation!
-
         if (input.Count() < 2)
             return 0;
 
         var coordinates = input
-            .Select(s => new Coordinate(s));
-
-        var groupedByX = coordinates
-            .GroupBy(c => c.X)
-            .OrderBy(g => g.Key)
+            .Select(s => new Coordinate(s))
             .ToList();
 
-        Console.Error.WriteLine($"X-Axis Groups: {groupedByX.Count}");
-
-        var minX = coordinates.Min(h => h.X);
-        var maxX = coordinates.Max(h => h.X);
-        var minY = coordinates.Min(h => h.Y);
-        var maxY = coordinates.Max(h => h.Y);
         var median = coordinates.OrderBy(c => c.Y).GetMedian().Y;
-        Console.Error.WriteLine($"Median: {median}");
 
         long result = 0;
 
-        // Add Distance from xMin  to xMax for Houses
-        result += (maxX - minX);
+        // Add main cable length ...
+        result += (coordinates.Max(h => h.X) - coordinates.Min(h => h.X));
 
-        // For Each House, Add Their Distance from Median
-        foreach (var houseGroup in groupedByX)
-        {
-            // Calculate Distance to each how up/down.
-                var houseCable = houseGroup.Select(h =>
-                {
-                    var toHouse = Math.Abs(h.Y - median);
-                    Console.Error.WriteLine($"{toHouse} to house {h}");
-                    return toHouse;
-                })
-                .Sum();
+        // ... and cable to each house.
+        var houseCabling = coordinates
+            .Select(c => Math.Abs(c.Y - median))
+            .Sum();
 
-                result += houseCable;
-        }
+        result += houseCabling;
 
         return result;
     }
@@ -84,17 +63,6 @@ public static class Extensions
 {
     public static T GetMedian<T>(this IEnumerable<T> data)
     {
-        // https://github.com/mathnet/mathnet-numerics/blob/master/src/Numerics/Statistics/ArrayStatistics.cs#L414
-        /*
-
-            var k = data.Length/2;
-            return data.Length.IsOdd()
-                ? SelectInplace(data, k)
-                : (SelectInplace(data, k - 1) + SelectInplace(data, k))/2.0;
-
-         */
-
-        var k = data.Count() / 2;
-        return data.ElementAt(k);
+        return data.ElementAt(data.Count() / 2);
     }
 }
