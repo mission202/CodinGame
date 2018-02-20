612de64048bef2e8d1eadc603928fd2b4676dd73
Rob Cooper <robc.the.geek@gmail.com>
2018-02-20 12:02:04 +0000

    WIP: SNAFU :)

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -5,27 +5,74 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
+public static class D
+{
+    public static Action<string> Print = message => Console.Error.WriteLine(message);
+    public static Action<string> Write = message => Console.WriteLine(message);
+    public static Func<string> Read = () => Console.ReadLine();
+    public static Func<int> ReadInt = () => int.Parse(Read());
+}
+
 public class Solution
 {
-    static Action<string> Print = message => Console.Error.WriteLine(message);
-    static Action<string> Write = message => Console.WriteLine(message);
-    static Func<string> Read = () => Console.ReadLine();
-    static Func<int> ReadInt = () => int.Parse(Read());
-
     static void Main(string[] args)
     {
-        int R = ReadInt();
-        int L = ReadInt();
+        int R = D.ReadInt();
+        int L = D.ReadInt();
 
-        Write(ConwaySequence.Find(R, L));
+        D.Write(ConwaySequence.Find(R, L));
     }
 }
 
 public static class ConwaySequence
 {
-    public static string Find(int start, int line)
+    public static string Find(int start, int targetLine)
     {
-        return "1 1 1 3 1 2 2 1 1 3 3 1 1 2 1 3 2 1 1 3 2 1 2 2 2 1";
-        return string.Empty;
+        var lines = new List<char[]> { new[] { start.ToString()[0] } };
+
+        for (int currentLine = 0; currentLine < targetLine; currentLine++)
+        {
+            var line = lines.Last();
+            D.Print($"On Line {currentLine}, Last Line: {string.Join(" ", line)}");
+
+            var current = line[0];
+            var count = 0;
+
+            for (int charIdx = 0; charIdx < line.Length; charIdx++)
+            {
+                // Same as Last? Add to Count
+                var next = line[charIdx];
+                if (next == current)
+                {
+                    count++;
+                    D.Print($"Current Char {next} same as last, incremented count to {count}.");
+                }
+                else
+                {
+                    current = next;
+                    count = 1;
+                }
+
+                // Encode
+                var chars = Enumerable.Range(0, count).Select(x => current).ToArray();
+                lines.Add(chars);
+                D.Print($"Added '{chars.SpacedOut()}' to lines. Count now at {lines.Count()}.");
+            }
+        }
+
+        return lines.SpacedOut(); // o_O
+    }
+}
+
+public static class Extensions
+{
+    public static string SpacedOut(this IEnumerable<char> chars)
+    {
+        return string.Join(" ", chars);
+    }
+
+    public static string SpacedOut(this IEnumerable<char[]> lines)
+    {
+        return string.Join(" ", lines.Select(l => l.SpacedOut()));
     }
 }
