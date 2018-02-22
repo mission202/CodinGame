80bc5eec8a4c62fd24205e2d1f926004f08bee3a
Rob Cooper <robc.the.geek@gmail.com>
2018-02-22 05:51:31 +0000

    First Tests Added

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace CodinGame.App
+public static class Solution
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string[] inputs = Console.ReadLine().Split(' ');
+        int L = int.Parse(inputs[0]);
+        int C = int.Parse(inputs[1]);
+        for (int i = 0; i < L; i++)
         {
+            string row = Console.ReadLine();
         }
+
+        Console.WriteLine("answer");
     }
 
-    public static class Solution
+    public static IEnumerable<string> Find(IEnumerable<string> input)
     {
-        public static bool Works => true;
+        return new string[] { "SOUTH", "SOUTH", "EAST", "EAST" };
     }
 }
--- CodinGame.Tests/Tests.cs
+++ CodinGame.Tests/Tests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CodinGame.App;
 using Xunit;
 
 namespace CodinGame.Tests
@@ -11,9 +10,19 @@ namespace CodinGame.Tests
     public class Tests
     {
         [Fact]
-        public void TestsWork()
+        public void SimpleMoves()
         {
-            Assert.True(Solution.Works);
+            var input = "5 5|#####|#@  #|#   #|#  $#|#####".Split('|');
+            var expected = "SOUTH|SOUTH|EAST|EAST".Split('|');
+            Assert.Equal(expected, Solution.Find(input));
+        }
+
+        [Fact]
+        public void Obstacles()
+        {
+            var input = "8 8|########|# @    #|#     X#|# XXX  #|#   XX #|#   XX #|#     $#|########".Split('|');
+            var expected = "SOUTH|EAST|EAST|EAST|SOUTH|EAST|SOUTH|SOUTH|SOUTH".Split('|');
+            Assert.Equal(expected, Solution.Find(input));
         }
     }
 }
