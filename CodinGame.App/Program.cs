2abbb3867b6355149ca40cfa2740edf628e2ef52
Rob Cooper <robc.the.geek@gmail.com>
2018-02-24 11:47:03 +0000

    File > New Challenge

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -1,20 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace CodinGame.App
+/**
+ * Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
+ **/
+class Player
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string[] inputs;
+        int projectCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < projectCount; i++)
         {
+            inputs = Console.ReadLine().Split(' ');
+            int a = int.Parse(inputs[0]);
+            int b = int.Parse(inputs[1]);
+            int c = int.Parse(inputs[2]);
+            int d = int.Parse(inputs[3]);
+            int e = int.Parse(inputs[4]);
         }
-    }
 
-    public static class Solution
-    {
-        public static bool Works => true;
+        // game loop
+        while (true)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                string target = inputs[0];
+                int eta = int.Parse(inputs[1]);
+                int score = int.Parse(inputs[2]);
+                int storageA = int.Parse(inputs[3]);
+                int storageB = int.Parse(inputs[4]);
+                int storageC = int.Parse(inputs[5]);
+                int storageD = int.Parse(inputs[6]);
+                int storageE = int.Parse(inputs[7]);
+                int expertiseA = int.Parse(inputs[8]);
+                int expertiseB = int.Parse(inputs[9]);
+                int expertiseC = int.Parse(inputs[10]);
+                int expertiseD = int.Parse(inputs[11]);
+                int expertiseE = int.Parse(inputs[12]);
+            }
+            inputs = Console.ReadLine().Split(' ');
+            int availableA = int.Parse(inputs[0]);
+            int availableB = int.Parse(inputs[1]);
+            int availableC = int.Parse(inputs[2]);
+            int availableD = int.Parse(inputs[3]);
+            int availableE = int.Parse(inputs[4]);
+            int sampleCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sampleCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int sampleId = int.Parse(inputs[0]);
+                int carriedBy = int.Parse(inputs[1]);
+                int rank = int.Parse(inputs[2]);
+                string expertiseGain = inputs[3];
+                int health = int.Parse(inputs[4]);
+                int costA = int.Parse(inputs[5]);
+                int costB = int.Parse(inputs[6]);
+                int costC = int.Parse(inputs[7]);
+                int costD = int.Parse(inputs[8]);
+                int costE = int.Parse(inputs[9]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("GOTO DIAGNOSIS");
+        }
     }
 }
