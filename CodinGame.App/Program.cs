1acc0c6e8a01e300da1565c7d739f8bb5f57e417
Rob Cooper <robc.the.geek@gmail.com>
2018-02-25 09:54:05 +0000

    Fugly Code, but Working!

--- CodinGame.App/Program.cs
+++ CodinGame.App/Program.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 
 public class Game
 {
+    public const int MAX_SAMPLES = 3;
+    public const int MAX_MOLECULES = 10;
+
     static void Main(string[] args)
     {
         string[] inputs;
@@ -15,54 +18,123 @@ public class Game
         {
             inputs = Console.ReadLine().Split(' ');
             var project = new MoleculeCollection(inputs.Select(int.Parse).ToArray());
-            Console.Error.WriteLine($"Project: {project}");
+            //Console.Error.WriteLine($"Project: {project}");
         }
+
+        Player player;
+        Player opponent;
+        List<SampleDataFile> samples;
 
         // game loop
         while (true)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                var input = Console.ReadLine();
-                var player = new Player(input);
-            }
+            player = new Player(Console.ReadLine());
+            opponent = new Player(Console.ReadLine());
 
             inputs = Console.ReadLine().Split(' ');
-
             var available = new MoleculeCollection(inputs.Select(int.Parse).ToArray());
-            Console.Error.WriteLine($"Available: {available}");
+            //Console.Error.WriteLine($"Available: {available}");
 
+            samples = new List<SampleDataFile>();
             int sampleCount = int.Parse(Console.ReadLine());
             for (int i = 0; i < sampleCount; i++)
             {
                 var input = Console.ReadLine();
+                Console.Error.WriteLine($"S {i} - {input}");
                 var sample = new SampleDataFile(input);
-                Console.Error.WriteLine($"Sample {sample.Id}: {sample.Cost}");
+                Console.Error.WriteLine($"Parsed {sample.Cost}");
+
+                samples.Add(sample);
+                //Console.Error.WriteLine($"Sample {sample.Id}: {sample.Cost}");
             }
 
-            Console.WriteLine("GOTO HELL");
+            var ai = new AI(samples, player);
+            Console.WriteLine(ai.GetNextAction());
         }
+    }
+}
+
+public class AI
+{
+    public List<SampleDataFile> Samples { get; }
+    public Player Player { get; }
+
+    public const int MAX_SAMPLES = 3;
+    public const int MAX_MOLECULES = 10;
+
+    public AI(List<SampleDataFile> samples, Player player)
+    {
+        Samples = samples;
+        Player = player;
+    }
+
+    public string GetNextAction()
+    {
+        // No Sample Data Files? Go to DIAGNOSIS
+        var heldByPlayer = Samples.Where(x => x.CarriedByPlayer).ToList();
+
+        // We've done grunt work - research away!
+        if (Player.Target == Modules.LABORATORY && heldByPlayer.Count > 0)
+        {
+            var toResearch = heldByPlayer.OrderByDescending(x => x.Health).First();
+            return $"CONNECT {toResearch.Id}";
+        }
+
+        if (heldByPlayer.Count() == 0)
+        {
+            if (Player.Target != Modules.DIAGNOSIS) return $"GOTO {Modules.DIAGNOSIS}";
+
+            // Connect and Get Best Sample
+            var bestSample = Samples.Where(x => x.InCloud).OrderByDescending(x => x.Health).First();
+            return $"CONNECT {bestSample.Id}";
+        }
+
+        // Got Files, Need Molecules? Go to MOLECULES
+        if (heldByPlayer.Count() > 0)
+        {
+            if (Player.Target != Modules.MOLECULES)
+                return $"GOTO {Modules.MOLECULES}";
+
+            // Connect and Types for Best Samples (up to max of 10);
+            var shoppingFor = heldByPlayer.OrderByDescending(x => x.Health).First();
+
+            if (Player.Storage.A < shoppingFor.Cost.A) return "CONNECT A";
+            if (Player.Storage.B < shoppingFor.Cost.B) return "CONNECT B";
+            if (Player.Storage.C < shoppingFor.Cost.C) return "CONNECT C";
+            if (Player.Storage.D < shoppingFor.Cost.D) return "CONNECT D";
+            if (Player.Storage.E < shoppingFor.Cost.E) return "CONNECT E";
+
+            Console.Error.WriteLine($"Shopping: {shoppingFor.Id} {shoppingFor.Cost} | Storage: {Player.Storage}");
+
+            // Got Molecules? Go to LABORATORY
+            return $"GOTO {Modules.LABORATORY}";
+        }
+
+        // Shouldn't really get here.
+        return "GOTO HELL";
     }
 }
 
 public class Player
 {
+    public string Target { get; private set; }
     public SampleDataFile[] SampleDataFiles { get; private set; }
-    public char[] Molecules { get; private set; }
+    public MoleculeCollection Storage { get; private set; }
+    public MoleculeCollection Expertise { get; private set; }
 
     public Player(string input)
     {
         var inputs = input.Split(' ');
-        string target = inputs[0];
+        Target = inputs[0];
 
         var ints = inputs.Skip(1).Select(int.Parse).ToArray();
         int eta = ints[1];
         int score = ints[2];
 
-        var storage = new MoleculeCollection(ints.Skip(2).Take(5).ToArray());
-        Console.Error.WriteLine($"Storage: {storage}");
-        var expertise = new MoleculeCollection(ints.Skip(7).Take(5).ToArray());
-        Console.Error.WriteLine($"Expertise: {storage}");
+        Storage = new MoleculeCollection(ints.Skip(2).Take(5).ToArray());
+        //Console.Error.WriteLine($"Storage: {Storage}");
+        Expertise = new MoleculeCollection(ints.Skip(7).Take(5).ToArray());
+        //Console.Error.WriteLine($"Expertise: {Expertise}");
     }
 }
 
@@ -89,7 +161,12 @@ public class MoleculeCollection
 public class SampleDataFile
 {
     public int Id { get; private set; }
+
     public int CarriedBy { get; private set; }
+    public bool CarriedByPlayer => CarriedBy == 0;
+    public bool CarriedByOpponent => CarriedBy == 1;
+    public bool InCloud => CarriedBy == -1;
+
     public int Rank { get; private set; }
     public string Gain { get; private set; }
     public int Health { get; private set; }
@@ -104,7 +181,7 @@ public class SampleDataFile
         Rank = int.Parse(inputs[2]);
         Gain = inputs[3];
         Health = int.Parse(inputs[4]);
-        Cost = new MoleculeCollection(inputs.Skip(4).Take(5).Select(int.Parse).ToArray());
+        Cost = new MoleculeCollection(inputs.Skip(5).Take(5).Select(int.Parse).ToArray());
     }
 }
 
--- CodinGame.Tests/Tests.cs
+++ CodinGame.Tests/Tests.cs
@@ -16,9 +16,20 @@ namespace CodinGame.Tests
         }
 
         [Fact]
-        public void CanPrioritiseSampleData()
+        public void ParseSampleData()
         {
-            ca
+            var sample = new SampleDataFile("0 -1 1 0 1 0 2 1 0 0");
+            Assert.Equal(0, sample.Id);
+            Assert.Equal(-1, sample.CarriedBy);
+            Assert.True(sample.InCloud);
+            Assert.Equal(1, sample.Rank);
+            Assert.Equal("0", sample.Gain);
+            Assert.Equal(1, sample.Health);
+            Assert.Equal(0, sample.Cost.A);
+            Assert.Equal(2, sample.Cost.B);
+            Assert.Equal(1, sample.Cost.C);
+            Assert.Equal(0, sample.Cost.D);
+            Assert.Equal(0, sample.Cost.E);
         }
     }
 }
