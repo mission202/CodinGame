6917e78d0cd9ecd92ecad9a8c17b9ad74933b8ec
Rob Cooper <rob.cooper@thetrainline.com>
2018-08-01 18:02:15 +0100

    First Hack of 'Just Attack'

--- code.js
+++ code.js
@@ -3,9 +3,22 @@
  * the standard input according to the problem statement.
  **/
 
+const state = {
+    turn: 0
+};
+
+const ACTIONS = {
+    summon: (id, message) => `SUMMON ${id} ${message}`,
+    attack: (myId, theirId, message) => `ATTACK ${myId} ${theirId} ${message}`,
+    directAttack: (myId, message) => `ATTACK ${myId} -1 ${message}`
+};
+
+let turn = 0;
 
 // game loop
 while (true) {
+    turn++;
+
     for (var i = 0; i < 2; i++) {
         var inputs = readline().split(' ');
         var playerHealth = parseInt(inputs[0]);
@@ -14,6 +27,8 @@ while (true) {
         var playerRune = parseInt(inputs[3]);
     }
     var opponentHand = parseInt(readline());
+
+    let cards = [];
     var cardCount = parseInt(readline());
     for (var i = 0; i < cardCount; i++) {
         var inputs = readline().split(' ');
@@ -28,10 +43,53 @@ while (true) {
         var myHealthChange = parseInt(inputs[8]);
         var opponentHealthChange = parseInt(inputs[9]);
         var cardDraw = parseInt(inputs[10]);
+
+        cards.push({
+            inputs: inputs,
+            cardNumber: cardNumber,
+            instanceId: instanceId,
+            location: location,
+            cardType: cardType,
+            cost: cost,
+            attack: attack,
+            defense: defense,
+            abilities: abilities,
+            myHealthChange: myHealthChange,
+            opponentHealthChange: opponentHealthChange,
+            cardDraw: cardDraw
+        });
     }
 
     // Write an action using print()
     // To debug: printErr('Debug messages...');
 
-    print('PASS');
+    // draft phase
+    if (turn < 31) {
+        print('PASS');
+        continue;
+    };
+
+    let actions = [];
+    let inhand = cards.filter(x => x.location === 0);
+
+    printErr(`Cards in Hand: ${inhand.length}`);
+
+    // TODO: Enough Mana for Summon - SUMMON!
+
+    let canSummon = inhand.filter(x => x.cost <= playerMana).sort((a, b) => a.cost < b.cost);
+    let manaLeft = playerMana;
+    for (let i = 0; i < canSummon.length; i++) {
+        let summoning = canSummon[i];
+        if (summoning.cost > manaLeft) break;
+        manaLeft -= summoning.cost;
+        actions.push(ACTIONS.summon(summoning.instanceId, `RELEASE THE ${summoning.instanceId}!`));
+    }
+
+    // TODO: Anything on Board - ATTACCCCKKK!!
+    let onboard = cards.filter(x => x.location === 1);
+    for(let creature of onboard) {
+        actions.push(ACTIONS.directAttack(creature.instanceId, 'ATTAACCCKK!!'));
+    }
+
+    print(actions.join(";"));
 }
